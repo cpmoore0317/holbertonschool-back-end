3c83fc0410f91b13a05c4180cf5e564a6ff93e6d
cpmoore0317 <kingpkayy@gmail.com>
2023-11-16 11:36:40 -0600

    Fixing imports

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,8 +4,9 @@ For a given employee ID,
 returns information about his/her TODO list progress.
 """
 import json
-from sys import argv
 import requests
+from sys import argv
+
 
 if __name__ == '__main__':
     # Base URL for the JSONPlaceholder API

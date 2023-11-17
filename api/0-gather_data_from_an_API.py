b65742741283dba7850966b53587e1ec17b68959
cpmoore0317 <kingpkayy@gmail.com>
2023-11-17 09:52:19 -0600

    Testing changes for task0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,33 +4,49 @@ For a given employee ID,
 returns information about his/her TODO list progress.
 """
 import requests
+import sys
 
-
-def get_employee_progress(employee_id):
-    """ Gets the employee's todo list progress """
+def get_employee_todo_progress(employee_id):
+    """
+    Retrieve and display the progress of an employee's completed tasks.
+    
+    Args:
+    - employee_id (int): The ID of the employee.
+    """
+    # API endpoint
     base_url = "https://jsonplaceholder.typicode.com"
-    user_endpoint = f"{base_url}/users/{employee_id}"
-    todo_endpoint = f"{base_url}/todos"
+    
+    # Construct URLs for employee and todo data
+    employee_url = f"{base_url}/users/{employee_id}"
+    todo_url = f"{base_url}/todos"
 
-    user_data = requests.get(user_endpoint).json()
-    todo_data = requests.get(todo_endpoint, params={"userId": employee_id}).json()
+    # Retrieve employee and todo data from the API
+    employee_info = requests.get(employee_url).json()
+    todo_list = requests.get(todo_url, params={"userId": employee_id}).json()
 
-    employee_name = user_data.get("name")
-    completed_tasks = [task["title"] for task in todo_data if task["completed"]]
-    num_completed_tasks = len(completed_tasks)
-    total_tasks = len(todo_data)
+    # Extract relevant information from employee data
+    employee_name = employee_info.get("name")
 
-    print(f"Employee {employee_name} has completed {num_completed_tasks}/{total_tasks} tasks:")
+    # Filter completed tasks from the todo list
+    completed_tasks = [task["title"] for task in todo_list if task["completed"]]
+
+    # Calculate the number of completed and total tasks
+    number_completed = len(completed_tasks)
+    number_total = len(todo_list)
+
+    # Display the employee's task progress
+    print(f"Employee {employee_name} has completed {number_completed}/{number_total} tasks:")
+    
+    # Display each completed task
     for task in completed_tasks:
-        print(f"\t{task}")
-
+        print(f"\t {task}")
 
 if __name__ == "__main__":
-    import sys
-
+    # Check if an employee ID is provided as a command-line argument
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
         sys.exit(1)
 
+    # Call the function with the provided employee ID
     employee_id = int(sys.argv[1])
-    get_employee_progress(employee_id)
+    get_employee_todo_progress(employee_id)
